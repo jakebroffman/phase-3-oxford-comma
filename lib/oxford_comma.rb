74dd46a3b3c06cd953ab9efb82b70ac1bcc4f0e7
jakebroffman <jake.broffman1088@gmail.com>
2023-06-06 17:32:01 -0400

    commit tree dd8e2d1f6292d400d4b057907485fccd5dd3cc57

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length == 2
+        array[0] << " and "
+        array.join
+    elsif array.length > 2
+        array[-1].prepend("and ")
+        array.join(", ")
+    else
+        array[0]
+    end
 end
